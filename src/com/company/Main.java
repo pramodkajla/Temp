fff7db1b567b87510e13fc5d5a973ab5713169d4
pramod kajla <pramod.kajla@gmail.com>
2021-06-24 17:53:36 +0530

    sms utility

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("working on sms utility");
+//        System.out.println("working on sms utility");
+        System.out.println("SMS utility work completed");
     }
+
 }
