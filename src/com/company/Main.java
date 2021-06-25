ec364eb3c278379e6e5841fea038b554c4e9b47d
pramod kajla <pramod.kajla@gmail.com>
2021-06-25 14:46:24 +0530

    email feature

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,5 +7,6 @@ public class Main {
         System.out.println("SMS utility work completed");
         System.out.println("Merging done with master");
         System.out.println("Email feature added.");
+        System.out.println("Rework done on email utility");
     }
 }
